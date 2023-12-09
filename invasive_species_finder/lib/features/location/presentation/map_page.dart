c7b18bb5df99e60a1c255f5ced5166854630f6ab
hangbozhang <59976512+hangbozhang@users.noreply.github.com>
2023-12-08 18:43:57 -1000

    markers added

--- invasive_species_finder/lib/features/location/presentation/map_page.dart
+++ invasive_species_finder/lib/features/location/presentation/map_page.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:invasive_species_finder/features/species/domain/species_collection.dart';
 import 'package:permission_handler/permission_handler.dart';
+
+import '../../species/domain/species.dart';
 
 class MapPage extends StatefulWidget {
   MapPage({
@@ -19,14 +23,27 @@ class MapPage extends StatefulWidget {
 
 class MapPageState extends State<MapPage> {
   final Completer<GoogleMapController> _controller = Completer();
-  late GoogleMapController _googleMapController;
+  GoogleMapController? _googleMapController;
   late GeolocatorPlatform geolocator;
-
+  BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
+  List<Marker> markers = [];
+  final LatLng _initialPosition = const LatLng(21.300730704104364, -157.81481745653235); // Initial map position
 
   @override
   void initState() {
     super.initState();
     _requestPermission(context);
+    retrievePlaces();
+    moveCameraToInitialPosition();
+  }
+
+  void moveCameraToInitialPosition() {
+    if (_googleMapController != null) {
+      _googleMapController?.animateCamera(CameraUpdate.newLatLngZoom(
+          _initialPosition,
+          14.0,
+      ));
+    }
   }
 
   Future<void> _requestPermission(BuildContext context) async {
@@ -63,7 +80,7 @@ class MapPageState extends State<MapPage> {
           desiredAccuracy: LocationAccuracy.high
       );
 
-      _googleMapController.animateCamera(CameraUpdate.newCameraPosition(
+      _googleMapController?.animateCamera(CameraUpdate.newCameraPosition(
         CameraPosition(
           target: LatLng(position.latitude, position.longitude),
           zoom: 10.0,
@@ -72,6 +89,39 @@ class MapPageState extends State<MapPage> {
 
     } catch (e) {
       print('Error: $e');
+    }
+  }
+
+  void retrievePlaces() async {
+    QuerySnapshot<Map<String, dynamic>> speciesLocations = await FirebaseFirestore.instance.collection('locations').get();
+    QuerySnapshot<Map<String, dynamic>> speciesData = await FirebaseFirestore.instance.collection('species').get();
+
+    List<Species> speciesList = speciesData.docs.map((doc){
+      return Species.fromJson(doc.data());
+    }).toList();
+
+    SpeciesCollection species = SpeciesCollection(speciesList);
+
+    for(QueryDocumentSnapshot<Map<String, dynamic>> speciesLocation in speciesLocations.docs) {
+      Map<String, dynamic> data = speciesLocation.data();
+      String id = speciesLocation.id;
+      String speciesID = data['speciesID'];
+      String name = species.getSpeciesName(speciesID);
+      double latitude = data['attitude'].toDouble();
+      double longitude = data['latitude'].toDouble();
+      markers.add(
+        Marker(
+          markerId: MarkerId(id),
+          position: LatLng(latitude, longitude),
+          infoWindow: InfoWindow(
+            title: name,
+          ),
+        ),
+      );
+    }
+
+    if (mounted) {
+      setState(() {});
     }
   }
 
@@ -84,10 +134,14 @@ class MapPageState extends State<MapPage> {
           zoom: 10.0,
         ),
         onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-          _googleMapController = controller;
-          _getUserLocation();
+          setState(() {
+            _controller.complete(controller);
+            _googleMapController = controller;
+            _getUserLocation();
+          });
         },
+        markers: Set.from(markers),
+        myLocationEnabled: true,
       ),
     );
   }
